e98fa09707dba330be6f504ce03ac63b92d70071
AtulAravindDas <atulbhaijaan@gmail.com>
2025-09-04 16:05:17 -0400

    Added an on_start event

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ import os
 load_dotenv()
 
 #print("Key loaded:", os.getenv("OPENAI_API_KEY"))  
+@cl.on_start
 
+async def on_start():
+    await cl.Message(content="Hi, what can I do for you today?!").send()
 @cl.on_message
 async def main(message: cl.Message):
     await cl.Message(content=f"Received: {message.content}").send()
