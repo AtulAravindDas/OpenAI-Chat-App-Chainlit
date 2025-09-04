ce81db7dc846bf201395f1240d5de55979fe37c7
AtulAravindDas <atulbhaijaan@gmail.com>
2025-09-04 16:01:42 -0400

    Sneaky errors

--- app.py
+++ app.py
@@ -8,6 +8,5 @@ load_dotenv()
 #print("Key loaded:", os.getenv("OPENAI_API_KEY"))  
 
 @cl.on_message
-async def main(message: str):
-    await cl.Message(content=f"Received:{message.content}",).send()
-
+async def main(message: cl.Message):
+    await cl.Message(content=f"Received: {message.content}").send()
