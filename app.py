c665063b085a48d48e40815a029df01d092a53ac
AtulAravindDas <atulbhaijaan@gmail.com>
2025-09-04 22:08:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,15 +6,15 @@ import os
 load_dotenv()
 
 #print("Key loaded:", os.getenv("OPENAI_API_KEY"))  
-'''@cl.on_chat_start
+@cl.on_chat_start
 
 async def on_start():
-    await cl.Message(content="Hi, what can I do for you today?!").send()''' #THis code snippet will be executed when the chat starts
+    await cl.Message(content="Hi, what can I do for you today?!").send() #THis code snippet will be executed when the chat starts
 @cl.on_message
 async def main(message: cl.Message):
     await cl.Message(content=f"Received: {message.content}").send()
 
-@cl.set_starters
+'''@cl.set_starters
 async def set_starters():
     return [
         cl.Starter(
@@ -39,4 +39,4 @@ async def set_starters():
             message="Write a text asking a friend to be my plus-one at a wedding next month. I want to keep it super short and casual, and offer an out.",
             icon="/public/write.svg",
         )
-    ]
+    ]'''
